8163af348a1ffbe177a62e17273e019ffada3905
YayyWon <yeangsshi@gmail.com>
2023-11-05 02:01:23 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-const express = require('express')  //express 모듈 불러오기
-const app = express()  //function 이용해서 새로운 express app 만들기
-const port = 5000   //5000번 포트를 백 서버로 사용
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const { User } = require('./models/User');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://yeangsshi:Wonkim0gi720@cluster0.avat7.mongodb.net/').then(() => console.log('connected')).catch(error => console.log(err))
+const config = require('./config/key');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+mongoose
+    .connect(config.mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('MongoDB Connected..');
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`);
+        });
+    })
+    .catch((error) => console.log(error));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!~~안녕하세요~')
-})
+    res.send('Hello World!~~안녕하세요~');
+});
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+app.post('/register', async (req, res) => {
+    // 회원 가입할 때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터베이스에 넣어준다.
+    const user = new User(req.body);
+
+    try {
+        const userInfo = await user.save();
+        res.status(200).json({ success: true });
+    } catch (err) {
+        res.status(500).json({ success: false, err });
+    }
+});
