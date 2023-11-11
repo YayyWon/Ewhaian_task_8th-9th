6dc55dde539636dcbdc9e1544fe7e48bb35d8f0c
YayyWon <yeangsshi@gmail.com>
2023-11-11 21:40:03 +0900

    두번째 커밋

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import LandingPage from './components/views/LandingPage/LandingPage'
+import LoginPage from './components/views/LoginPage/LoginPage'
+import RegisterPage from './components/views/RegisterPage/RegisterPage'
+import AboutPage from './components/views/AboutPage/AboutPage'
+import SkillsPage from './components/views/SkillsPage/SkillsPage'
+import WorksPage from './components/views/WorksPage/WorksPage'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/register" component={RegisterPage} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/skills" component={SkillsPage} />
+          <Route exact path="/works" component={WorksPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://yeangsshi:Wonye0611@cluster0.avat7.mongodb.net/'
+}
